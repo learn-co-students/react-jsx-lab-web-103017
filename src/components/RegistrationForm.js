8d2a41faeab63c78ab92d4f3eb866c6187867af5
RochelLevi <rochlevi@gmail.com>
2018-01-07 21:33:15 -0500

    Done.

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class RegistrationForm extends React.Component {
+  render() {
+    return (
+      <form class="" action="index.html" method="post">
+        <input type="text" name="" value="" />
+        <input type="password" name="" value="" />
+        <button type="submit" name="button"></button>
+      </form>
+    );
+  }
+}
+
+export default RegistrationForm;
